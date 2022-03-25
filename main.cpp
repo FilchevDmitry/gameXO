37ba5ed747a66210cc5c0b9c95188665ce01ee38
DmityFilchev <filchev_dmitry@mail.ru>
2022-03-25 14:11:00 +0300

    1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
-
+#include <string>
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+   std::string str1;
+   std::string str2;
+   std::string str3;
+   std::cout<<"Input playing field in the format\n";
+   std::cout<<"X..\n";
+   std::cout<<".O.\n";
+   std::cout<<"..X\n";
+   std::cin >> str1;
+   std::cin >> str2;
+   std::cin >> str3;
+   std::cout <<std::endl;
+    std::cout <<str1<<std::endl;
+    std::cout <<str2<<std::endl;
+    std::cout <<str3<<std::endl;
     return 0;
 }
