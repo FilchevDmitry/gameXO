5d5c728a261021a86926ad397a4b30b7467236b9
DmityFilchev <filchev_dmitry@mail.ru>
2022-03-28 13:32:15 +0300

    1

--- main.cpp
+++ main.cpp
@@ -1,20 +1,29 @@
 #include <iostream>
 #include <string>
-std::string str(){
-    std::string str;
-    std::cin >> str;
-    return str;
+bool str(std::string str){
+    if (str.length()==3) {
+        for (int i = 0; i < 3; ++i) {
+            if(str[i]!='O'&& str[i]!='X'&& str[i]!='.' )
+                return false;
+        }
+        return true;
+    }
+    else
+    return false;
 }
 int main() {
-   std::cout<<"Input the playing field in the format. Valid characters 'X' 'O' and '.'\n";
-   std::cout<<"\t\tX..\n";
-   std::cout<<"\t\t.O.\n";
-   std::cout<<"\t\t..X\n";
-    for (int i = 0; i < 3; ++i) {
-        str();
-    }
-
-
-
+   std::cout<<"Input the playing field in the format.\nValid characters 'X' 'O' and '.'\n";
+   std::cout<<"X..\n";
+   std::cout<<".O.\n";
+   std::cout<<"..X\n";
+   std::string str1;
+   std::string str2;
+   std::string str3;
+   std::cin >> str1;
+   std::cin >> str2;
+   std::cin >> str3;
+    std::cout<<str(str1);
+    std::cout<<str(str2);
+    std::cout<<str(str3);
     return 0;
 }
